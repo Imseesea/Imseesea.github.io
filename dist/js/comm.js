9933d7aae8c03db39a04d4042159b3509270ec6d
Imseesea <136055408+Imseesea@users.noreply.github.com>
2023-11-02 18:18:35 +0900

    contents-box

---
+++ dist/js/comm.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+  $("#fullpage").fullpage({
+    //options here
+    licenseKey: "OPEN-SOURCE-GPLV3-LICENSE",
+    autoScrolling: true,
+    scrollHorizontally: true,
+    navigation: true,
+    loopBottom: true,
+    sectionSelector: "section",
+    scrollingSpeed: 800,
+    scrollbars: false,
+  });
+});
