cb9bda37735ad9130423b29c74ea9ea5b58c41a9
Imseesea <136055408+Imseesea@users.noreply.github.com>
2023-11-03 18:20:04 +0900

    section

--- dist/js/comm.js
+++ dist/js/comm.js
@@ -11,3 +11,28 @@ $(document).ready(function () {
     scrollbars: false,
   });
 });
+
+$(document).ready(function () {
+  $("#fullPage").fullpage({
+    autoScrolling: true,
+    scrollHorizontally: true,
+    navigation: true,
+    anchors: [
+      "firstPage",
+      "secondPage",
+      "thirdPage",
+      "fourthPage",
+      "fifthPage",
+      "sixthpage",
+    ],
+    afterLoad: function (anchorLink, index) {
+      if (index == 1) {
+        $(".home-wrap").css({
+          width: "450px",
+          height: "450px",
+        });
+        //
+      }
+    },
+  });
+});
