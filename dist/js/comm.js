d98f211b5696b58d6a8c52edf60d43e5f847b588
Imseesea <136055408+Imseesea@users.noreply.github.com>
2023-11-16 18:02:35 +0900

    nav,img

--- dist/js/comm.js
+++ dist/js/comm.js
@@ -9,14 +9,6 @@ $(document).ready(function () {
     sectionSelector: "section",
     scrollingSpeed: 800,
     scrollbars: false,
-  });
-});
-
-$(document).ready(function () {
-  $("#fullPage").fullpage({
-    autoScrolling: true,
-    scrollHorizontally: true,
-    navigation: true,
     anchors: ["firstPage", "secondPage", "3rdPage", "4thpage", "5thpage"],
     afterLoad: function (anchorLink, index) {
       if (index == 1) {
@@ -27,30 +19,5 @@ $(document).ready(function () {
         //
       }
     },
-
-    // onLeave: function (index, nextIndex, derection) {
-    //   console.log(nextIndex);
-    //   if (nextIndex == 3) {
-    //     $(".mockup_mini").addClass("mockup_ani");
-    //     $(".mini_bg").addClass("hover_bg_ani");
-    //   } else {
-    //     $(".mockup_mini").removeClass("mockup_ani");
-    //     $(".mini_bg").removeClass("hover_bg_ani");
-    //   }
-    //   if (nextIndex == 4) {
-    //     $(".mockup_melon").addClass("mockup_ani");
-    //     $(".melon_bg").addClass("hover_bg_ani");
-    //   } else {
-    //     $(".mockup_melon").removeClass("mockup_ani");
-    //     $(".melon_bg").removeClass("hover_bg_ani");
-    //   }
-    //   if (nextIndex == 5) {
-    //     $(".mockup_nobrand").addClass("mockup_ani");
-    //     $(".nobrand_bg").addClass("hover_bg_ani");
-    //   } else {
-    //     $(".mockup_nobrand").removeClass("mockup_ani");
-    //     $(".nobrand_bg").removeClass("hover_bg_ani");
-    //   }
-    // },
   });
 });
